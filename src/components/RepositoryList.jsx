8cbcd5057e261891e96be3f589c517faae25e48a
anketola <39833064+anketola@users.noreply.github.com>
2025-05-02 21:03:31 +0300

    Finish exercise 10.24

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,9 +1,10 @@
-import { FlatList, View, StyleSheet } from 'react-native';
+import { FlatList, View, StyleSheet, TextInput } from 'react-native';
 import RepositoryItem from './RepositoryItem';
 import theme from '../theme';
 import useRepositories from '../hooks/useRepositories';
 import {Picker} from '@react-native-picker/picker';
 import { useState } from 'react';
+import { useDebounce } from 'use-debounce';
 
 const styles = StyleSheet.create({
   container: {
@@ -13,16 +14,34 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colors.mainBackground,
       height: 10
 },
+input: {
+  borderWidth: 2,
+  borderRadius: 9,
+  backgroundColor: '#ffffff',
+  height: 60,
+  padding: 20,
+  marginLeft: 15,
+  marginRight: 15,
+  marginTop: 15
+},
 });
 
-const SortingHeader = ({ handleSortChange, sortingType }) => {
+const SortingHeader = ({ handleSortChange, sortingType, handleSearchChange, searchString  }) => {
+  
   
   return(
-    <View style={{ flex: 1, backgroundColor: theme.colors.mainBackground, height: 60, justifyContent: 'center'}}>      
+    <View style={{ flex: 1, backgroundColor: theme.colors.mainBackground, height: 120, justifyContent: 'center'}}>      
+      <TextInput
+        style={[styles.input]}
+        placeholder="Search"
+        value={searchString}
+        onChangeText={(itemValue) => {
+          handleSearchChange(itemValue);
+        }}
+      />
       <Picker
         selectedValue={sortingType}
         onValueChange={(itemValue, itemIndex) => {
-          //setSortingSettings(itemValue)
           handleSortChange(itemValue)
         }}>
         <Picker.Item label="Select an item.." value="" enabled={false} />
@@ -37,7 +56,13 @@ const SortingHeader = ({ handleSortChange, sortingType }) => {
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-export const RepositoryListContainer = ({ repositories, sortingType, handleSortChange }) => {
+export const RepositoryListContainer = ({ 
+  repositories, 
+  sortingType, 
+  searchString, 
+  handleSortChange, 
+  handleSearchChange
+}) => {
   const repositoryNodes = repositories
     ? repositories.edges.map((edge) => edge.node)
     : [];
@@ -48,7 +73,13 @@ export const RepositoryListContainer = ({ repositories, sortingType, handleSortC
       ItemSeparatorComponent={ItemSeparator}
       renderItem={({item}) => <RepositoryItem data={item} />}
       keyExtractor={item => item.id}
-      ListHeaderComponent={<SortingHeader sortingType={sortingType} handleSortChange={handleSortChange} />}
+      ListHeaderComponent={
+      <SortingHeader 
+        sortingType={sortingType} 
+        handleSortChange={handleSortChange}  
+        searchString={searchString} 
+        handleSearchChange={handleSearchChange}
+      />}
     />
   );
 };
@@ -56,7 +87,9 @@ export const RepositoryListContainer = ({ repositories, sortingType, handleSortC
 const RepositoryList = () => {
   
   const [sortingType, setSortingType] = useState("latest");
-  
+  const [searchString, setSearchString] = useState("");
+  const [debounceValue] = useDebounce(searchString, 500);
+
   let queryArguments;
 
   if (sortingType === "latest") {
@@ -75,10 +108,23 @@ const RepositoryList = () => {
       orderDirection: "ASC"
     }
   } 
+  if (searchString !== '') {
+    queryArguments = {
+      ...queryArguments,
+      searchKeyword: debounceValue
+    }
+  }
+
 
   const { repositories } = useRepositories(queryArguments);
 
-  return <RepositoryListContainer repositories={repositories} sortingType={sortingType} handleSortChange={setSortingType}/>;
+  return <RepositoryListContainer 
+    repositories={repositories} 
+    sortingType={sortingType} 
+    searchString={searchString} 
+    handleSearchChange={setSearchString} 
+    handleSortChange={setSortingType}
+    />;
 };
 
 
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -3,14 +3,17 @@ import { GET_REPOSITORIES } from '../graphql/queries';
 
 const useRepositories = ( sortSettings ) => {
     console.log('sortSettings', sortSettings);
+    
+    const variables = {
+      orderBy: sortSettings.orderBy,
+      orderDirection: sortSettings.orderDirection,
+      ...(sortSettings.searchKeyword && { searchKeyword: sortSettings.searchKeyword })
+    };
+
     const {loading, data, refetch } = useQuery(GET_REPOSITORIES, {
         fetchPolicy: 'cache-and-network',
-        variables: {
-          orderBy: sortSettings.orderBy,
-          orderDirection: sortSettings.orderDirection
-        }
-        
-        });
+        variables        
+    });
     const repositories = data?.repositories
     
 
